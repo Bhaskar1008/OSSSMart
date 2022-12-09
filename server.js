73aedb9d5a89a61899ff92ec0886b29e488511f6
Geetha Madhuri <geethagorantla20@gmail.com>
2022-12-09 18:31:00 +0530

    Category Rote Updated

--- server.js
+++ server.js
@@ -2,24 +2,26 @@ const express = require('express');
 const app = express();
 
 const db = require('./config/db.config')
-const Address=require('./models/AddressDetails.schema')
-const CartFavouriteDetails=require('./models/CartFavouriteDetails.schema')
-const Categories=require('./models/Categories.schema')
-const Customer=require('./models/Customer.schema')
-const ItemConfiguration=require('./models/ItemConfiguration.schema')
-const OrderDetails=require('./models/OrderDetails.schema')
-const reviews=require('./models/reviews.schema')
-const Thumbnails=require('./models/Thumbnails.schema')
+const Address = require('./models/AddressDetails.schema')
+const CartFavouriteDetails = require('./models/CartFavouriteDetails.schema')
+const Categories = require('./models/Categories.schema')
+const Customer = require('./models/Customer.schema')
+const ItemConfiguration = require('./models/ItemConfiguration.schema')
+const OrderDetails = require('./models/OrderDetails.schema')
+const reviews = require('./models/reviews.schema')
+const Thumbnails = require('./models/Thumbnails.schema')
 app.use(express.json());
 
 const userRoute = require('./routes/customerRoute')
-const addressRoute=require('./routes/addressRoute')
+const addressRoute = require('./routes/addressRoute')
+const categoryRoute=require('./routes/categoryRoute')
 app.use('/api/users', userRoute)
-app.use('/api/address',addressRoute)
+app.use('/api/address', addressRoute)
+app.use('/api/category',categoryRoute)
 app.get('/', (req, res) => {
     res.send('Server Working' + port)
 })
 const port = process.env.PORT || 5000;
-app.listen(port, () => 'Server running on port '+port);
+app.listen(port, () => 'Server running on port ' + port);
 
 
